5e1bd1aca6006b0bb8c62aa17ea75199a4e43b57
CRamoysi <romain.civel@gmail.com>
2016-03-02 11:26:17 +0100

    Premier raytracer

---
+++ main.lua
@@ -0,0 +1,307 @@
+love.graphics.setDefaultFilter("nearest") -- set le filtre d'affichage par defaut des images
+local Utils = require("utils")
+
+
+
+local classicmap = {
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+				{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+				{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+}
+
+local emptymap = {
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+}
+
+local grottemap = {
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+}
+
+local map2 = {
+				{8,8,8,8,8,8,8,8,8,8,8,4,4,6,4,4,6,4,6,4,4,4,6,4},
+				{8,0,0,0,0,0,0,0,0,0,8,4,0,0,0,0,0,0,0,0,0,0,0,4},
+				{8,0,3,3,0,0,0,0,0,8,8,4,0,0,0,0,0,0,0,0,0,0,0,6},
+				{8,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6},
+				{8,0,3,3,0,0,0,0,0,8,8,4,0,0,0,0,0,0,0,0,0,0,0,4},
+				{8,0,0,0,0,0,0,0,0,0,8,4,0,0,0,0,0,6,6,6,0,6,4,6},
+				{8,8,8,8,0,8,8,8,8,8,8,4,4,4,4,4,4,6,0,0,0,0,0,6},
+				{7,7,7,7,0,7,7,7,7,0,8,0,8,0,8,0,8,4,0,4,0,6,0,6},
+				{7,7,0,0,0,0,0,0,7,8,0,8,0,8,0,8,8,6,0,0,0,0,0,6},
+				{7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,6,0,0,0,0,0,4},
+				{7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,6,0,6,0,6,0,6},
+				{7,7,0,0,0,0,0,0,7,8,0,8,0,8,0,8,8,6,4,6,0,6,6,6},
+				{7,7,7,7,0,7,7,7,7,8,8,4,0,6,8,4,8,3,3,3,0,3,3,3},
+				{2,2,2,2,0,2,2,2,2,4,6,4,0,0,6,0,6,3,0,0,0,0,0,3},
+				{2,2,0,0,0,0,0,2,2,4,0,0,0,0,0,0,4,3,0,0,0,0,0,3},
+				{2,0,0,0,0,0,0,0,2,4,0,0,0,0,0,0,4,3,0,0,0,0,0,3},
+				{1,0,0,0,0,0,0,0,1,4,4,4,4,4,6,0,6,3,3,0,0,0,3,3},
+				{2,0,0,0,0,0,0,0,2,2,2,1,2,2,2,6,6,0,0,5,0,5,0,5},
+				{2,2,0,0,0,0,0,2,2,2,0,0,0,2,2,0,5,0,5,0,0,0,5,5},
+				{2,0,0,0,0,0,0,0,2,0,0,0,0,0,2,5,0,5,0,5,0,5,0,5},
+				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5},
+				{2,0,0,0,0,0,0,0,2,0,0,0,0,0,2,5,0,5,0,5,0,5,0,5},
+				{2,2,0,0,0,0,0,2,2,2,0,0,0,2,2,0,5,0,5,0,0,0,5,5},
+				{2,2,2,2,1,2,2,2,2,2,2,1,2,2,2,5,5,5,5,5,5,5,5,5}
+}
+
+local map = map2
+
+
+
+local px, py = 22,12 -- position de la camera
+local dx, dy = -1,0 -- direction de la camera
+local planeX,planeY = 0,0.66 -- ratio de la camera
+local time, oldTime = 0,0 -- timer
+local w = love.graphics.getWidth() -- largeur de l'écran
+local h = love.graphics.getHeight() -- hauteur de l'écran
+local deltaMove, deltaRotation = 0,0 -- deplacement et rotation
+local spdMove, spdRotation = 5,3 -- vitesse de deplacement et rotation
+
+-- keyboard
+local toV,toH = 0,0 -- direction de deplacement recuperer en fonction des touches du clavier appuyé
+local toUp, toLeft, toDown, toRight = "up", "left", "down", "right"
+local toUpD, toLeftD, toDownD, toRightD = 1, -1, -1, 1
+
+-- =============================================================================
+
+function love.load()
+
+
+end
+
+-- =============================================================================
+
+function love.update(delta_time)
+	deltaMove = delta_time*spdMove
+	deltaRotation = delta_time*spdRotation
+
+	if toV ~= 0 then -- deplacement
+		if map[math.floor(px+toV*dx*deltaMove)][math.floor(py)] == 0 then
+			px = px + toV*dx*deltaMove
+		end
+		if map[math.floor(px)][math.floor(py + toV*dy*deltaMove)] == 0 then
+			py = py + toV*dy*deltaMove
+		end
+	end
+		
+	if toH ~= 0 then -- rotation
+		local oldDX = dx
+		dx = dx*math.cos(-toH*deltaRotation) - dy*math.sin(-toH*deltaRotation)
+		dy = oldDX*math.sin(-toH*deltaRotation) + dy*math.cos(-toH*deltaRotation)
+		local oldPlaneX = planeX
+		planeX = planeX*math.cos(-toH*deltaRotation) - planeY*math.sin(-toH*deltaRotation)
+		planeY = oldPlaneX*math.sin(-toH*deltaRotation) + planeY*math.cos(-toH*deltaRotation)
+		
+	end	
+	
+end
+
+-- =============================================================================
+
+function love.draw()
+	love.graphics.clear({0,0,0})
+	
+	for x = 0,w,1 do
+		local cx = 2*x / w - 1 -- position de la colonne par rapport au centre de l’écran
+		local rayPosX, rayPosY = px, py -- position de départ
+		--rayDir = vecteur de direction + (vecteur de l’écran * position de la colonne par rapport au centre de l’écran) 
+		local rayDirX = dx + planeX*cx
+		local rayDirY = dy + planeY*cx
+		
+		local mx, my = math.floor(px), math.floor(py) -- case courante
+		local sideDistX, sideDistY -- longueur du rayon
+		-- longueur du rayon entre chaque intersection
+		local deltaDistX = math.sqrt(1 + (rayDirY*rayDirY)/(rayDirX*rayDirX))
+		local deltaDistY = math.sqrt(1 + (rayDirX*rayDirX)/(rayDirY*rayDirY))
+		
+		local perWallDist -- distance corrigée du rayon
+		
+		local stepX, stepY -- direction de l'étape ({-1,1})
+		
+		local hit = 0 -- un mur a été touché?
+		local side -- le mur etait sur quel axe?  0 => X, 1 => Y 
+		
+		-- calcule le vecteur de direction et la longueur entre deux segments
+		if rayDirX < 0 then
+			stepX = -1
+			sideDistX = (rayPosX - mx) * deltaDistX -- distance
+		else
+			stepX = 1
+			sideDistX = (mx + 1 - rayPosX) * deltaDistX
+		end
+		if rayDirY < 0 then
+			stepY = -1
+			sideDistY = (rayPosY -my) * deltaDistY
+		else
+			stepY = 1
+			sideDistY = (my + 1 - rayPosY) * deltaDistY
+		end
+	
+	
+		-- lancé de rayon
+		while hit == 0 do
+			if sideDistX < sideDistY then
+				sideDistX = sideDistX + deltaDistX
+				mx = mx + stepX
+				side = 0
+			else
+				sideDistY = sideDistY + deltaDistY
+				my = my + stepY
+				side = 1
+			end
+			if map[mx][my] > 0 then
+				hit = 1
+			end
+		end
+	
+		-- calcul distance du rayon perpendiculaire (un ratio diferent de 1 creer un fish eye)
+		if side == 0 then
+			perWallDist = math.abs((mx - rayPosX + (1 - stepX) / 2) / rayDirX)
+		else
+			perWallDist = math.abs((my - rayPosY + (1 - stepY) / 2) / rayDirY)
+		end
+
+		local lineHeight = math.abs(math.floor(h / perWallDist)) -- hauteur de la ligne à afficher à l'ecran
+	
+		-- calculs des pixel max haut et max bas de la colonne à tracer. 
+		local drawStart = -lineHeight/2 + h/2
+		local drawEnd = lineHeight/2 + h/2
+		if drawStart < 0 then drawStart = 0 end
+		if drawEnd >= h then drawEnd = h-1 end
+	
+		local color = {255,0,0} -- couleur de base d'un mur
+		if side == 1 then -- on assombris les murs lateraux
+			for k,v in ipairs(color) do
+				color[k] = v/2
+			end
+		end
+		love.graphics.setColor(color)
+		love.graphics.line(x,drawStart,x,drawEnd)
+	
+	end
+end
+
+-- =============================================================================
+
+
+function love.mousepressed(x, y, button, istouch)
+
+
+
+end
+
+-- =============================================================================
+
+-- Gestion du clavier
+function love.keypressed(key)
+	if key == "escape" then -- pour quitter (mieux qu'un bouton quitter mageul !)
+		love.event.quit()
+	end
+
+	if key == toUp then
+		toV = toV+toUpD
+	end
+	if key == toDown then
+		toV = toV+toDownD
+	end
+	if key == toLeft then
+		toH = toH+toLeftD
+	end
+	if key == toRight then
+		toH = toH+toRightD
+	end   
+   
+   
+end
+
+function love.keyreleased(key)
+	if key == toUp then
+		toV = toV-toUpD
+	end
+	if key == toDown then
+		toV = toV-toDownD
+	end
+	if key == toLeft then
+		toH = toH-toLeftD
+	end
+	if key == toRight then
+		toH = toH-toRightD
+	end 
+end
+
+
+
+
+
+
+
+
+
+
